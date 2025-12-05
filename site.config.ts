a5fbfb73d7ecf27992232f55d0bfcf2244f4348b
yong-mahalo <yonghaohu.mahalo@gmail.com>
2025-12-05 11:42:36 +0100

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,15 +2,15 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '2bdb256d3a8280dc83ffee1ab49b9d7c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  name: 'Yonghao Hu',
+  domain: 'yonghaohu.com',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
